b8964276e4a7ce5f399477bbd8387c21dbbe0223
arun <arunsudhakar18@gmail.com>
2024-10-17 18:52:01 +0530

    appjs updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Signup from "./components/signup"
+import Signup from "./components/signup";
 import Signin from "./components/login";
 export default function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element = {<Home/>}/>
-          <Route path="/signup" element = {<Signup/>}/>
-          <Route path="/signin" element = {<Signin/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/login" element={<Signin />} />
         </Routes>
       </div>
     </Router>
