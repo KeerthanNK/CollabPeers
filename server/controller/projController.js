2e267927e091899267b85be8528c47355ab4eb27
arun <arunsudhakar18@gmail.com>
2024-10-17 10:32:08 +0530

    getAll proj posted

--- server/controller/projController.js
+++ server/controller/projController.js
@@ -27,7 +27,9 @@ export const createProj = async (req, res) => {
     }
 
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    console.log(decoded);
+
+    const Email = decoded.email;
+    console.log(Email + "email");
 
     const proj = await Proj.create({
       projectname,
@@ -37,11 +39,31 @@ export const createProj = async (req, res) => {
       collegename: decoded.collegename,
       year: decoded.year,
       searchingFor,
+      email: Email,
     });
 
     res.status(201).json({ msg: "Project created successfully", proj });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getUserProject = async (req, res) => {
+  try {
+    const Token = req.headers.authorization?.split(" ")[1];
+    if (!Token) {
+      return res.status(401).json({ error: "No token provided" });
+    }
+
+    const decoded = jwt.verify(Token, process.env.SECRET_KEY);
+
+    const Email = decoded.email;
+
+    const getAll = await Proj.find({ email: Email });
+
+    res.status(200).json({ getAll });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" + err });
   }
 };
