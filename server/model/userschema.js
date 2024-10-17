38ae193deff695510d439fa8a5831005167bbdcc
arun <arunsudhakar18@gmail.com>
2024-10-17 09:09:13 +0530

    changes in schema

--- server/model/userschema.js
+++ server/model/userschema.js
@@ -3,53 +3,44 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    //unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  cpassword: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  collegename: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: String,
-    required: true,
-  },
-  course: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
-      },
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-});
+    email: {
+      type: String,
+      required: true,
+      //unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    cpassword: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    collegename: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    course: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 12);
@@ -57,15 +48,6 @@ userSchema.pre("save", async function (next) {
   }
   next();
 });
-userSchema.methods.generateAuthToken = async function () {
-  try {
-    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
-    this.tokens = this.tokens.concat({ token: token });
-    await this.save();
-    return token;
-  } catch (err) {
-    console.log(err);
-  }
-};
+
 const Users = mongoose.model("User", userSchema);
 export default Users;
